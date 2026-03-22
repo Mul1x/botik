cbaf25c953e166ca5e383a40127b7ea99a6ab266
Mul1x <159061027+Mul1x@users.noreply.github.com>
2026-03-22 13:53:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8674178298:AAFuv0b3fTCANT-ADw3INJtxRKN-WCCLryA")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "garantmoskowbot")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "G1ftGuardBot")
 ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "7687750743,8072028362").split(",")]
