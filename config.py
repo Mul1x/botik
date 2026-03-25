35d9b7faa7146e21ee5b9c179a467d866266977e
Mul1x <159061027+Mul1x@users.noreply.github.com>
2026-03-25 23:35:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8790464708:AAFbSZ9ofxx9H2Ef0O1wX6CGTLVFe7yIIf8")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8455279912:AAFoWu_2-qxq-BoJUjzwXV1tcRcBnBptkhs")
 BOT_USERNAME = os.getenv("BOT_USERNAME", "GGuardRobot")
 SUPER_ADMIN_IDS = [int(x) for x in os.getenv("SUPER_ADMIN_IDS", "8717189451,8072028362").split(",")]
